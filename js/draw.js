dc6eb10623bbc6999642686fd4df433bf002fbe8
Konstantin Simeonov <kon.simeonov@protonmail.com>
2018-08-22 01:56:37 +0300

    Refactor in draw function

--- js/draw.js
+++ js/draw.js
@@ -1,15 +1,17 @@
-const draw = field_info => {
+const _draw = container => field_info => {
 	const { length } = field_info;
-	const container = document.getElementById('maze-container');
+	const borders = ['top', 'left', 'right', 'bottom'];
+	const black_border = '1px solid black';
 
 	for (let i = 0; i < length; ++i) {
 		for (let j = 0; j < length; ++j) {
 			const div = document.createElement('div');
-			const borders = ['top', 'left', 'right', 'bottom'];
 			Object.assign(
 				div.style,
 				...field_info[i][j]
-					.map((has_wall, ix) => !has_wall && { [`border-${borders[ix]}`]: '1px solid black' })
+					.map((has_path, ix) => !has_path && {
+						[`border-${borders[ix]}`]: black_border
+					})
 					.filter(Boolean)
 			);
 			container.appendChild(div);
