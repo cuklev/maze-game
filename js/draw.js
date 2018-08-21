7bede993acb6ce3ef1d2277b630b87133fb315db
Konstantin Simeonov <kon.simeonov@protonmail.com>
2018-08-22 01:57:38 +0300

    Added fill function to colorize cells

--- js/draw.js
+++ js/draw.js
@@ -20,5 +20,22 @@ const _draw = container => field_info => {
 	
 };
 
-draw(maze);
+const _fill = container => columns => (r, c) => {
+	const element = container.children[r * columns + c];
+	const color = element.style.backgroundColor || 'rgb(255, 0, 0)';
+
+	const rgb = Array.of;
+	const mask = eval(color).reduce((m, c, i) => m | (Boolean(c) << i), 0) + 1;
+	const new_rgb = [0, 1, 2].map(i => (mask >> i & 1) * 255);
+
+	element.style.backgroundColor = `rgb(${new_rgb.join(', ')})`;
+};
+
+const init_picasso = maze => {
+	const container = document.getElementById('maze-container');
+	return {
+		fill: _fill(container)(maze[0].length),
+		draw: _draw(container)
+	};
+};
 
