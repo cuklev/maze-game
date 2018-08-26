6be46053ad3d2c324ac5ba1117c3d0cd3968dc4b
Kristiyan Tsaklev <cuki@cukii.me>
2018-08-26 16:21:09 +0300

    Update generator.js to output valid .js directly

--- generator.js
+++ generator.js
@@ -1,37 +1,50 @@
-const MAZE_WIDTH = 42;
-const MAZE_HEIGHT = 25;
-
 const dr = [-1, 0, 0, 1];
 const dc = [0, -1, 1, 0];
 
-const entry_row = Math.random() * MAZE_HEIGHT | 0;
-const exit_row = Math.random() * MAZE_HEIGHT | 0;
+const generate = (width, height) => {
+	const entry_row = Math.random() * height | 0;
+	const exit_row = Math.random() * height | 0;
 
-const mazeOutput = Array.from({length: MAZE_HEIGHT})
-	.map(r => Array.from({length: MAZE_WIDTH}).map(c => [false, false, false, false]));
+	const mazeOutput = Array.from({length: height})
+		.map(r => Array.from({length: width}).map(c => [false, false, false, false]));
 
-mazeOutput[entry_row][0][1] = true;
-mazeOutput[exit_row][MAZE_WIDTH - 1][2] = true;
+	mazeOutput[entry_row][0][1] = true;
+	mazeOutput[exit_row][width - 1][2] = true;
 
-const visited = Array.from({length: MAZE_HEIGHT}).map(() => []);
+	const visited = Array.from({length: height}).map(() => []);
 
-const isValidCell = (r, c) => r >= 0 && r < MAZE_HEIGHT && c >= 0 && c < MAZE_WIDTH;
+	const isValidCell = (r, c) => r >= 0 && r < height && c >= 0 && c < width;
 
-const dfs = (r, c) => {
-	visited[r][c] = true;
+	const dfs = (r, c) => {
+		visited[r][c] = true;
 
-	[0, 1, 2, 3]
-		.sort(() => Math.random() - 0.5)
-		.filter(i => isValidCell(r + dr[i], c + dc[i]))
-		.forEach(i => {
-			if(!visited[r + dr[i]][c + dc[i]]) {
-				mazeOutput[r][c][i] = true;
-				mazeOutput[r + dr[i]][c + dc[i]][i ^ 3] = true;
-				dfs(r + dr[i], c + dc[i]);
-			}
-		});
+		[0, 1, 2, 3]
+			.sort(() => Math.random() - 0.5)
+			.filter(i => isValidCell(r + dr[i], c + dc[i]))
+			.forEach(i => {
+				if(!visited[r + dr[i]][c + dc[i]]) {
+					mazeOutput[r][c][i] = true;
+					mazeOutput[r + dr[i]][c + dc[i]][i ^ 3] = true;
+					dfs(r + dr[i], c + dc[i]);
+				}
+			});
+	};
+
+	dfs(0, 0);
+
+	return mazeOutput;
 };
 
-dfs(0, 0);
+const showArray = (x) => {
+	if(!Array.isArray(x)) return x + '';
+	return '[' + x.map(y => showArray(y)).join(',') + ']';
+};
 
-console.log(mazeOutput);
+const mazes = [0, 1, 2, 3].map(_ => generate(42, 25));
+const output = `
+// the maze is 2d array of cell descriptions
+// each cell is an array of 4 bools: [up, left, right, down]
+
+const mazes = ${showArray(mazes)};`;
+
+require('fs').writeFileSync('js/mazes.js', output);
