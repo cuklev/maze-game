3f83f76bfa6f712f19e81df9ee6bc1d11136381d
Kristiyan Tsaklev <cuki@cukii.me>
2018-09-02 16:14:23 +0300

    Fix minor inconsistencies

--- js/draw.js
+++ js/draw.js
@@ -1,14 +1,14 @@
 const init_picasso = (() => {
 	const _unit = unit => value => `${value}${unit}`;
 	const [_px, _pct] = [`px`, `%`].map(_unit);
-	
+
 	const _draw = container => maze => {
 		const borders = [`top`, `left`, `right`, `bottom`]
-		const cell_size = (parseInt(container.clientWidth) / maze[0].length);
+		const cell_size = parseInt(container.clientWidth) / maze[0].length;
 		const st = { minWidth: _pct(100 / maze[0].length), maxWidth: _pct(100 / maze[0].length) };
 		const cell_pct_size = _pct(100 / maze[0].length);
 		document.styleSheets[0].insertRule(`.maze-cell { max-width: ${cell_pct_size}; min-width: ${cell_pct_size} }`);
-	
+
 		for (let i = 0; i < maze.length; ++i) {
 			const row = Object.assign(document.createElement(`div`), { className: `maze-row` });
 			for (let j = 0; j < maze[i].length; ++j) {
@@ -27,7 +27,7 @@ const init_picasso = (() => {
 			container.appendChild(row);
 		}
 	};
-	
+
 	const _fill = container => columns => (r, c) => {
 		const element = container.children[r].children[c];
 		const rgba = Array.of;
@@ -35,7 +35,7 @@ const init_picasso = (() => {
 		xs.push(Math.min(0.9, xs.pop() - -0.3));
 		element.style.backgroundColor = `rgba(${xs.join(', ')})`;
 	};
-	
+
 	return (container, maze) => ({
 		fill: _fill(container)(maze[0].length),
 		draw: _draw(container)
