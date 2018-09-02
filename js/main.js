0f83d238ad53825466b467817428c2f50e0ebade
Kristiyan Tsaklev <cuki@cukii.me>
2018-09-02 16:43:32 +0300

    Wrap main.js in IIFE

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-const editor = ace.edit('editor');
-editor.setTheme('ace/theme/monokai');
-editor.session.setMode("ace/mode/javascript");
-const get_solution = () => `
-        (() => {
-                ${editor.getValue()}
-                return step;
-        })();
-`;
+(() => {
+	const editor = ace.edit('editor');
+	editor.setTheme('ace/theme/monokai');
+	editor.session.setMode("ace/mode/javascript");
+	const get_solution = () => `
+		(() => {
+			${editor.getValue()}
+			return step;
+		})();
+	`;
 
-const start = () => init_mazes().map((c, i) => play(c)(mazes[i], get_solution()));
+	const start = () => init_mazes().map((c, i) => play(c)(mazes[i], get_solution()));
 
-document.getElementById('start-btn').addEventListener('click', start);
+	document.getElementById('start-btn').addEventListener('click', start);
+}();
