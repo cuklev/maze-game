97f428643f7d16ef91b37a8e862cb16325a7b0d5
Kristiyan Tsaklev <cuki@cukii.me>
2018-09-09 21:07:59 +0300

    Load initial "solution" code from main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@
 	const editor = ace.edit('editor');
 	editor.setTheme('ace/theme/monokai');
 	editor.session.setMode("ace/mode/javascript");
+	editor.setValue(`const step = (up, left, right, down, from) => {
+  const from_index = ['left', 'up', 'right', 'down'].indexOf(from);
+
+  const allowed = [up, right, down, left, up, right, down, left];
+  return ['up', 'right', 'down', 'left', 'up', 'right', 'down', 'left']
+      .find((dir, i) => i >= from_index && allowed[i]);
+};`);
+
 	const get_solution = () => `
 		(() => {
 			${editor.getValue()}
