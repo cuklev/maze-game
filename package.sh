7bf0b8a4918fb5eed791b65a44040d1b7940e29c
Konstantin Simeonov <kon.simeonov@protonmail.com>
2018-09-13 03:38:41 +0300

    Add package script

---
+++ package.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+readonly DIR="$(dirname $(readlink -f $0))"
+cd "$DIR"
+
+npm i
+
+zip -r maze.zip \
+	assets/ \
+	js/ \
+	css/ \
+	index.html \
+	node_modules/ace-builds/src-min-noconflict/
+
+echo "Maze packaged to $DIR/maze.zip"
